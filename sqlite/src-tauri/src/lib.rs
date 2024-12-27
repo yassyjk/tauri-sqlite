fad8b8c00f5745b0debdaacea9887636b296fbc5
yassyjk <yassyjk@gmail.com>
2024-12-27 22:15:31 +0900

    sqlite.dbをアプリのpathから生成するように修正

--- sqlite/src-tauri/src/lib.rs
+++ sqlite/src-tauri/src/lib.rs
@@ -1,4 +1,6 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
+use tauri_plugin_fs::FsExt;
 pub mod database;
 
 #[tauri::command]
@@ -10,10 +12,21 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .setup(|_app| {
-            database::init().map_err(|e| e.to_string())?;
+        .plugin(tauri_plugin_fs::init())
+        .setup(|app| {
+            let fs_scope = app.fs_scope();
+            fs_scope.allow_directory(app.path().app_data_dir().unwrap(), true)?;
+            let app_dir = app.path().app_data_dir().unwrap();
+            let db_path = app_dir.join(database::TAURI_DB);
+
+            database::init(&db_path.to_str().unwrap()).map_err(|e| e.to_string())?;
+
             Ok(())
         })
+        // .setup(|_app| {
+        //     database::init().map_err(|e| e.to_string())?;
+        //     Ok(())
+        // })
         .invoke_handler(tauri::generate_handler![greet, database::signup_user])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
